7d2cce3ced959ff4c817979e5c7ffa8cd964abea
Aadi1909 <aadarshtyagi124@gmail.com>
2025-12-27 22:45:51 +0530

    Fixed a critical bug in service class import statement

--- src/main/java/com/example/telegram_bot/service/JapaneseCorrectionService.java
+++ src/main/java/com/example/telegram_bot/service/JapaneseCorrectionService.java
@@ -1,7 +1,7 @@
 package com.example.telegram_bot.service;
 
 
-import jakarta.ws.rs.core.HttpHeaders;
+import org.springframework.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
